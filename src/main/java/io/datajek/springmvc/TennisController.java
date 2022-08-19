fbe09872c56d02d94c8b7f331f3efde7539e29d4
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-08-20 03:24:24 +0800

    add view

--- src/main/java/io/datajek/springmvc/TennisController.java
+++ src/main/java/io/datajek/springmvc/TennisController.java
@@ -2,15 +2,12 @@ package io.datajek.springmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TennisController {
 
     @RequestMapping(value = "/")
-    @ResponseBody
     public String welcome() {
-        System.out.println("coming");
-        return "Welcome to the Tennis Player database from Spring MVC!";
+        return "main-menu";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/main-menu.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Main Menu</title>
+</head>
+<body>
+    <h2> Spring MVC - Tennis Player Database </h2>
+</body>
+</html>
